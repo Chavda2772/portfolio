4992988ecbc9c15cd65ae661883f13f9a904de82
Mahesh Chavda <66986475+Chavda2772@users.noreply.github.com>
2021-04-18 13:17:30 +0530

    navtigation Home about skills and portfolio responsive design created

--- main.js
+++ main.js
@@ -1,4 +1,29 @@
-// let btnPortfolio = document.getElementById('btnShowMore');
-// btnPortfolio.addEventListener('click',function(e){
-//     window.location.replace('projects/');
-// });
+let btnMenuClose = document.getElementById("menuClose");
+let btnMenuOpen = document.getElementById("menuOpen");
+let navigation = document.querySelector("nav .navigation");
+let navigationLinks = document.querySelectorAll("#headerLinks li");
+
+//For Opening Menu in mobile view
+btnMenuOpen.addEventListener("click", function (e) {
+  btnMenuOpen.style.display = "none";
+  btnMenuClose.style.display = "block";
+
+  navigation.classList.add("active");
+});
+
+//For Closing Menu in mobile view
+btnMenuClose.addEventListener("click", function (e) {
+  btnMenuClose.style.display = "none";
+  btnMenuOpen.style.display = "block";
+
+  navigation.classList.remove("active");
+});
+
+//Close mobile menu on item selected
+navigationLinks.forEach((link, idx) => {
+  link.addEventListener("click", () => {
+    btnMenuClose.style.display = "none";
+    btnMenuOpen.style.display = "block";
+    navigation.classList.remove("active");
+  });
+});
